5f7cb0457445b9dce36a3d0e16eea376ac06958b
streltcov <streltsov83@yandex.ru>
2021-04-06 17:17:57 +0300

    update msg2cartopy command

--- hrit2image.py
+++ hrit2image.py
@@ -9,7 +9,9 @@ from glob import glob
 import numpy as np
 import pandas as pd
 import click
+import matplotlib
 import matplotlib.pyplot as plt
+import geopandas as gpd
 from cartopy.crs import Mercator, PlateCarree
 
 from satpy import Scene
@@ -29,6 +31,27 @@ CHANNELS = {
 PROJECTIONS = {
     'mercator': Mercator,
     'platecarree': PlateCarree 
+}
+
+GRAYSCALE_PALETTE = {
+    1: '#202020',
+    2: '#282828',
+    3: '#303030',
+    4: '#383838',
+    5: '#404040',
+    6: '#484848',
+    7: '#505050',
+    8: '#585858',
+    9: '#686868',
+    10: '#787878',
+    11: '#888888',
+    12: '#989898',
+    13: '#A8A8A8',
+    14: '#B8B8B8',
+    15: '#C8C8C8',
+    16: '#D8D8D8',
+    17: '#E8E8E8',
+    18: '#F8F8F8'
 }
 
 ########################################################################################################################
@@ -119,6 +142,9 @@ def msg_info(path):
 def msg2cartopy(path, channel="IR_108", projection="mercator", tolerance=1.0):
     """ Plot MSG HRIT file via Cartopy library """
 
+    matplotlib.use('Agg')
+    plt.ioff()
+
     channel = CHANNELS.get(channel, "IR_108")
     filenames = glob(path + "*")
 
@@ -127,16 +153,41 @@ def msg2cartopy(path, channel="IR_108", projection="mercator", tolerance=1.0):
 
     values = np.array(scn[channel].values)
 
-    # lons, lats = __define_swath_area(scn, channel)
-    lons = np.array([list(map(__convert_coordinate, [float(x) for x in scn[channel]['x']])), ] * 3712).reshape(3712 **2)
-    lats = np.array([list(map(__convert_coordinate, [float(y) for y in scn[channel]['y']])), ] * 3712).T.reshape(3712 ** 2)
+    lons = np.array([list(map(__convert_coordinate, [float(x) for x in scn[channel]['x']])), ] * 3712).reshape(3712 ** 2)
+    lats = np.array([list(map(__convert_coordinate, [float(y) for y in scn[channel]['y']])), ] * 3712).reshape(3712 ** 2)
 
-    values = list(values.reshape(3712 ** 2))
+    values = values.reshape(3712 ** 2)
 
-    df = pd.DataFrame.from_records([{'value': values, 'latitude': lats, 'longitude': lons}])
-    print(df.columns)
-    print(df.head())
-    # :levels = __create_levels(df)
+    data = np.stack([values[:-1], lons[:-1], lats[:-1]])
+    data = data.T
+
+    columns = ['value', 'longitude', 'latitude']
+
+    df = pd.DataFrame(data=data, index=np.arange(1, 3712 ** 2), columns=columns)
+
+    df= df.dropna()
+
+    levels = __create_levels(df)
+
+    ax = plt.axes(projection=Mercator())
+
+    ax.coastlines()
+    ax.stock_img()
+
+    gdf_1 = gpd.GeoDataFrame(data=levels[1], geometry=gpd.points_from_xy(levels[1].longitude, levels[1].latitude))
+    gdf_2 = gpd.GeoDataFrame(data=levels[2], geometry=gpd.points_from_xy(levels[2].longitude, levels[2].latitude))
+    gdf_3 = gpd.GeoDataFrame(data=levels[3], geometry=gpd.points_from_xy(levels[3].longitude, levels[3].latitude))
+
+    print(len(gdf_1))
+    print(len(gdf_2))
+    print(len(gdf_3))
+
+    gdf_1.plot(ax=ax, color=GRAYSCALE_PALETTE[1])
+    gdf_2.plot(ax=ax, color=GRAYSCALE_PALETTE[2])
+    gdf_3.plot(ax=ax, color=GRAYSCALE_PALETTE[3])
+
+    plt.savefig('sample.png')
+    plt.show()
 
 
 ########################################################################################################################
